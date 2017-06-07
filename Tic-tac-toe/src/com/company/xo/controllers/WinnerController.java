6595b8c0219606c8ed274fab00f7a378caf5bd05
marek2332 <markid2398@gmail.com>
2017-06-07 15:24:34 +0200

    Create WinnerController.java

---
+++ Tic-tac-toe/src/com/company/xo/controllers/WinnerController.java
@@ -0,0 +1,12 @@
+package com.company.xo.controllers;
+
+import com.company.xo.model.Field;
+
+public class WinnerController {
+
+    public String getWinner(final Field field) {
+        return "X";
+    }
+
+
+}
